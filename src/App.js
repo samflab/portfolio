39535ef43feaba8340239efa0c1681b858b77295
masudha.meher@telekom-digital.com <masudha.meher@telekom-digital.com[alias]>
2025-05-01 23:38:18 +0530

    modify: browser-router-to-hash-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import './components/styles/App.css'
@@ -37,7 +37,7 @@ const App = () => {
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 root.render(
-  <BrowserRouter>
+  <HashRouter> {/* ✅ Changed from <BrowserRouter> */}
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
